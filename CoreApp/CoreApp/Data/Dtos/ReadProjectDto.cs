e6d78f3ec5e87f155a5f85cdd930317e9ccc8442
Kacperacy <51511929+Kacperacy@users.noreply.github.com>
2021-12-16 09:32:18 +0100

    ReadDtos updated
    In ReadDtos I have changed normal objects to ReadDtos so it isn't recurrent.

--- CoreApp/CoreApp/Data/Dtos/ReadProjectDto.cs
+++ CoreApp/CoreApp/Data/Dtos/ReadProjectDto.cs
@@ -4,6 +4,7 @@ namespace CoreApp.Data.Dtos;
 
 public class ReadProjectDto
 {
+    public int Id { get; set; }
     public string Title { get; set; }
     public string? Description { get; set; }
     public ReadSectionDto Section { get; set; }
--- CoreApp/CoreApp/Data/Dtos/ReadStudentDto.cs
+++ CoreApp/CoreApp/Data/Dtos/ReadStudentDto.cs
@@ -4,6 +4,7 @@ namespace CoreApp.Data.Dtos;
 
 public class ReadStudentDto
 {
+    public int Id { get; set; }
     public string Indeks { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
@@ -12,6 +13,6 @@ public class ReadStudentDto
     public string? StudiesTag { get; set; }
     public string? Description { get; set; }
 
-    public virtual List<Role>? Roles { get; set; }
-    public virtual List<Project>? Projects { get; set; }
-}
+    public virtual List<ReadRoleDto>? Roles { get; set; }
+    public virtual List<ReadProjectDto>? Projects { get; set; }
+}   
